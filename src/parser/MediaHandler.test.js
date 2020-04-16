8d64b47bb9f966fdc0ca2e18035df6ebe59ac6bb
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-04-17 01:52:44 +0200

    Fixed minor problems with previous commit

--- src/parser/MediaHandler.test.js
+++ src/parser/MediaHandler.test.js
@@ -1,17 +1,6 @@
 import * as MediaHandler from "./MediaHandler";
 import Media from "../entities/Media";
 
-// const mockUploadMedia = jest.spyOn(MediaHandler, "uploadMedia");
-// 
-// mockUploadMedia.mockImplementation((media) => {
-//     if(media instanceof Media) {
-//         media.setUrl("correct.com");
-//         return 0;
-//     } else {
-//         // error.
-//         return -1;
-//     }
-// });
 const spyOn = jest.spyOn(MediaHandler, "uploadMedia");
 
 test("UploadMedia Fail", () => {
@@ -21,14 +10,6 @@ test("UploadMedia Fail", () => {
         expect(spyOn).toHaveBeenCalled();
     }
 });
-
-// test("UploadMedia Correct", () => {
-//     let m = new Media("photo", "My Photo.jpg", "ID1");
-//     let media = MediaHandler.uploadMedia(m);
-//     expect(media).toEqual(0);
-//     expect(m.getUrl()).toEqual("correct.com");
-//     expect(mockUploadMedia).toHaveBeenCalled();
-// });
 
 test("UploadMedia Correct", () => {
     let m = new Media("photo", "My Photo.jpg", "ID1");
--- src/parser/__mocks__/PodHandler.js
+++
@@ -1,6 +0,0 @@
-export const mockStoreMedia = jest.fn();
-const mock = jest.fn().mockImplementation(() => {
-  return {storeMedia: mockStoreMedia};
-});
-
-export default mock;
