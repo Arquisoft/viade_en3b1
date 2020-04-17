2c66cf0ff92c0acf1680d0852ba9763facdbc915
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-04-18 01:57:27 +0200

    EditableMap tests

--- src/components/map/EditableMap.js
+++ src/components/map/EditableMap.js
@@ -4,49 +4,67 @@ import { Map, Polyline, TileLayer, Marker } from 'react-leaflet';
 export class EditableMap extends Component {
 
     constructor() {
-		super();
-        this.state = { points: [] };
+        super();
+        this.state = {
+            points: []
+        };
         this.firstPoint = true;
-        
-	}
+
+    }
+
+    componentDidMount() {
+        this.setState({ points: [] });
+    }
 
     addPoint = (e) => {
-		this.state.points.push(e.latlng);
-        this.setState({ points: this.state.points.slice() });
-        
-        if(this.firstPoint === true) {
+        this.state.points.push(e.latlng);
+        const { points } = this.state;
+        // this.setState({ points: points.slice() });
+        this.state.points = points;
+
+        if (this.firstPoint === true) {
             this.props.openNotif("You can move a point by dragging it and delete it just clicking on it.", 'info');
             this.firstPoint = false;
         }
     }
 
     updatePoint = (event) => {
-		var id = event.target.options.marker_index;
-		var position = event.target.getLatLng();
-		const { points } = this.state;
-
-		points[id] = position;
-		this.setState({ points: points.slice() });
-	}
-
-	removePoint = (event) => {
         var id = event.target.options.marker_index;
+        var position = event.target.getLatLng();
+        this.updatePointById(id, position);
+    }
+
+    updatePointById = (id, position) => {
+        const { points } = this.state;
+
+        points[id] = position;
+        this.setState({ points: points.slice() });
+    }
+
+    removePoint = (event) => {
+        var id = event.target.options.marker_index;
+        this.removePointById(id);
+    }
+
+    removePointById = (id) => {
         const { points } = this.state;
         points.splice(id, 1);
         this.setState({ points: points.slice() });
+    }
 
-	}
-    
     getTrackPoints() {
         return this.state.points.slice();
     }
 
     render() {
         const position = [43.3619145, -5.8493887];
+        const { points } = this.state;
 
         return (
             <React.Fragment>
                 <Map
+                    data-testid="test-map-leaflet"
+                    id="test-map-leaflet"
                     center={position}
                     zoom={12}
                     style={{ width: "34.5rem", height: "20rem" }}
@@ -57,10 +75,11 @@ export class EditableMap extends Component {
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
 
-                    <Polyline positions={this.state.points} color='#09012f' />
+                    <Polyline positions={points} color='#09012f' />
 
-                    {this.state.points.map((position, index) =>
+                    {points.map((position, index) =>
                         <Marker
+                            key={index}
                             marker_index={index}
                             position={position}
                             draggable={true}
