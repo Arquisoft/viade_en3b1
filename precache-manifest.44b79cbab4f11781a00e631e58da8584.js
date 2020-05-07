67bd3c6265c45ecdfb061e08f2fcc692b6797f03
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-07 00:22:57 +0000

    Deploy Arquisoft/viade_en3b1 to github.com/Arquisoft/viade_en3b1.git:gh-pages

---
+++ precache-manifest.44b79cbab4f11781a00e631e58da8584.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f217d0166fe2d00f23779bc1c65979f1",
+    "url": "/viade_en3b1/index.html"
+  },
+  {
+    "revision": "ad6b5c23f06f47640d2c",
+    "url": "/viade_en3b1/static/css/2.3bb66fbf.chunk.css"
+  },
+  {
+    "revision": "894ca644a85437e449b3",
+    "url": "/viade_en3b1/static/css/main.deb5a4b7.chunk.css"
+  },
+  {
+    "revision": "ad6b5c23f06f47640d2c",
+    "url": "/viade_en3b1/static/js/2.59e88b15.chunk.js"
+  },
+  {
+    "revision": "80055ed007ed68f08391e69cd861fd04",
+    "url": "/viade_en3b1/static/js/2.59e88b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894ca644a85437e449b3",
+    "url": "/viade_en3b1/static/js/main.9afc1998.chunk.js"
+  },
+  {
+    "revision": "b6e1883c58e3a9df58cf",
+    "url": "/viade_en3b1/static/js/runtime-main.3fdf479e.js"
+  },
+  {
+    "revision": "85123741a62f9285d44ec3c306562e60",
+    "url": "/viade_en3b1/static/media/404.85123741.svg"
+  },
+  {
+    "revision": "d5ce39d81504d5c90400fb82b1cb822e",
+    "url": "/viade_en3b1/static/media/AboutData.d5ce39d8.svg"
+  },
+  {
+    "revision": "371613d1775810d68c734293f3dbd533",
+    "url": "/viade_en3b1/static/media/AboutInterop.371613d1.svg"
+  },
+  {
+    "revision": "af251772fe3b2ae7c3f7ec51817f9418",
+    "url": "/viade_en3b1/static/media/AboutMap.af251772.svg"
+  },
+  {
+    "revision": "df224c632d3e8c32385a262709d3a64c",
+    "url": "/viade_en3b1/static/media/AboutMe.df224c63.svg"
+  },
+  {
+    "revision": "f2c905a94d879d607a63794d5336305c",
+    "url": "/viade_en3b1/static/media/AboutViaDe.f2c905a9.svg"
+  },
+  {
+    "revision": "a78010bd6ea3675bb038a3ac27339108",
+    "url": "/viade_en3b1/static/media/HelpSupport.a78010bd.svg"
+  },
+  {
+    "revision": "91d3c9f35d738ae967ceadc661b29c2a",
+    "url": "/viade_en3b1/static/media/Proxima Nova Bold.91d3c9f3.ttf"
+  },
+  {
+    "revision": "151bfd040f66484fda825c10a61278ef",
+    "url": "/viade_en3b1/static/media/arrows.151bfd04.svg"
+  },
+  {
+    "revision": "c1e55643d597bc5a3029b07d8bfa6ebe",
+    "url": "/viade_en3b1/static/media/camera-retro.c1e55643.svg"
+  },
+  {
+    "revision": "fb216ff34012b083e8fe5cfd30fc77d0",
+    "url": "/viade_en3b1/static/media/camera.fb216ff3.svg"
+  },
+  {
+    "revision": "19c911577d370dae04bb2d2409b2e53f",
+    "url": "/viade_en3b1/static/media/inrupt.19c91157.svg"
+  },
+  {
+    "revision": "21d2fd21969b26089003a8198afc76b5",
+    "url": "/viade_en3b1/static/media/logo_1.21d2fd21.svg"
+  },
+  {
+    "revision": "3a6f0f912470da89830703a49a67e9e4",
+    "url": "/viade_en3b1/static/media/logo_2.3a6f0f91.svg"
+  },
+  {
+    "revision": "0509229b84c7dec2f94621fe1f236191",
+    "url": "/viade_en3b1/static/media/map.0509229b.svg"
+  },
+  {
+    "revision": "17e8f762ed80269329a2f02a9776dd14",
+    "url": "/viade_en3b1/static/media/mountain.17e8f762.svg"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/viade_en3b1/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/viade_en3b1/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/viade_en3b1/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/viade_en3b1/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/viade_en3b1/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/viade_en3b1/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/viade_en3b1/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/viade_en3b1/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/viade_en3b1/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/viade_en3b1/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/viade_en3b1/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/viade_en3b1/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/viade_en3b1/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/viade_en3b1/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/viade_en3b1/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/viade_en3b1/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/viade_en3b1/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/viade_en3b1/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/viade_en3b1/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/viade_en3b1/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/viade_en3b1/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/viade_en3b1/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/viade_en3b1/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/viade_en3b1/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "db94fd8bd9bbc2c725fc4aaa79d2ec36",
+    "url": "/viade_en3b1/static/media/server.db94fd8b.svg"
+  },
+  {
+    "revision": "245eff999196b6d3418e42ef5dc98b50",
+    "url": "/viade_en3b1/static/media/signal.245eff99.svg"
+  },
+  {
+    "revision": "a1390d205f45cc4b5ed110ba4d680f13",
+    "url": "/viade_en3b1/static/media/solidcommunity.a1390d20.png"
+  }
+]);
